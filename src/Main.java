9ffd237b3af40fba0f2eb72061f4ac71cfced90a
Pooky12332 <pooky12332@gmail.com>
2024-11-27 19:36:26 +0000

    Update Main.java
    - Added validation for textfile names (Should change to its own method)
    - Made changes to the "AES" class
    - Corrected some bugs in the menu's

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Main {
   public static void main(String[] args) {
     startMenu();
   }
 
+  // Main menu class
   public static void startMenu() {
     Scanner kb = new Scanner(System.in);
-
     System.out.print("\n1. Encrypt a file\n2. Decrypt a file\n3. Exit\n: ");
+
     int uin = kb.nextInt();
+
     if (uin == 1) {
-      System.out.println("Enter a file name: ");
+      encryptMenu();
     } else if (uin == 2) {
       System.out.println("Enter a file name: ");
     } else if (uin == 3) {
@@ -21,5 +24,27 @@ public class Main {
       System.out.println("Invalid input. Try again.");
       startMenu();
     }
+  }
+
+  // Encrypting the file
+  public static void encryptMenu() {
+    Scanner kb = new Scanner(System.in);
+    String filename = "";
+
+    // Validating the filename
+    try {
+      System.out.print("Enter a file name: ");
+      filename = kb.nextLine();
+
+      if ((filename.isBlank()) || (filename.contains(" ")) || (!filename.contains(".txt"))) {
+        System.out.println("Invalid input! Please enter a valid file name.");
+        filename = "";
+        encryptMenu();
+      }
+    } catch (InputMismatchException e) {
+      System.out.println("Invalid input! Please enter a valid file name.");
+    }
+
+    System.out.print(filename);
   }
 }
