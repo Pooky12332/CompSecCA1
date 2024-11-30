ae54697fd5466d775900e73dfc5efa3d2db1879c
Pooky <pooky12332@gmail.com>
2024-11-30 19:05:39 +0000

    Updated Main
    - Updated the menus so that they actually looks nice
    - Made methods for reused code
    - Cleaned some more bugs
    - Other misc changes

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
   // Main menu class
   public static void startMenu() {
     Scanner kb = new Scanner(System.in);
-    System.out.print("\n1. Encrypt a file\n2. Decrypt a file\n3. Exit\n: ");
+    System.out.print("\n[1] Encrypt a file\n[2] Decrypt a file\n[3] Exit\n[-] : ");
 
     int uin = kb.nextInt();
 
@@ -22,10 +22,10 @@ public class Main {
     } else if (uin == 2) {
       decryptMenu();
     } else if (uin == 3) {
-      System.out.println("Exiting...");
+      System.out.println("[✔] Exiting...");
       System.exit(0);
     } else {
-      System.out.println("Invalid input. Try again.");
+      System.out.println("[!] Invalid input. Try again.");
       startMenu();
     }
   }
@@ -38,16 +38,16 @@ public class Main {
 
     // Validating the filename
     try {
-      System.out.print("Enter a filename: ");
+      System.out.print("[-] Enter a filename: ");
       filename = kb.nextLine();
 
       if ((filename.isBlank()) || (filename.contains(" ")) || (!filename.contains(".txt"))) {
-        System.out.println("Invalid input! Please enter a valid file name.");
+        System.out.println("[!] Invalid input! Please enter a valid file name.");
         filename = "";
         encryptMenu();
       }
     } catch (InputMismatchException e) {
-      System.out.println("Invalid input! Please enter a valid file name.");
+      System.out.println("[!] Invalid input! Please enter a valid file name.");
       encryptMenu();
     }
 
@@ -61,16 +61,7 @@ public class Main {
     }
 
     // Reading the given file
-    String fileContents = "";
-
-    try (Scanner inFile = new Scanner(new File(filename))) {
-      while (inFile.hasNextLine()) {
-        fileContents += inFile.nextLine() + "\n";
-      }
-    } catch (FileNotFoundException e) {
-      System.out.println("Given file not found.");
-      startMenu();
-    }
+    String fileContents = readFile(filename);
 
     // Encrypting the file and placing it into the output
     String enContents = aes.encrypt(fileContents, key);
@@ -80,16 +71,15 @@ public class Main {
       try {
         outFile.createNewFile();
       } catch (IOException e) {
-        System.out.println("Couldnt create new file!");
+        System.out.println("[!] Couldnt create output file! Create 'ciphertext.txt' in the correct directory.");
       }
     }
 
-    try (PrintWriter w = new PrintWriter(new PrintWriter("ciphertext.txt"))) {
-      w.println(enContents);
-      System.out.println("File has been encrypted successfully\nKey: " + key + "\nOutput is located in 'ciphertext.txt'");
-    } catch (FileNotFoundException e) {
-      System.out.println("Given file not found.");
-      startMenu();
+    // Write to the given filename
+    if (writeFile("ciphertext.txt", enContents)) {
+      System.out.println("[✓] File has been encrypted successfully\n[├] Key: " + key + "\n[└] Output is located in 'ciphertext.txt'");
+    } else {
+      System.out.println("[!] Couldnt write to file! Make sure theres a 'ciphertext.txt' file in the directory.");
     }
 
     startMenu();
@@ -104,39 +94,30 @@ public class Main {
 
     // Get input and validate
     try {
-      System.out.print("Enter a file name: ");
+      System.out.print("[-] Enter a file name: ");
       filename = kb.nextLine();
 
       if ((filename.isBlank()) || (filename.contains(" ")) || (!filename.contains(".txt"))) {
-        System.out.println("Invalid input! Please enter a valid file name.");
+        System.out.println("[!] Invalid input! Please enter a valid file name.");
         filename = "";
         encryptMenu();
       }
     } catch (InputMismatchException e) {
-      System.out.println("Invalid input! Please enter a valid file name.");
+      System.out.println("[!] Invalid input! Please enter a valid file name.");
       decryptMenu();
     }
 
     // Get key and validate
     try {
-      System.out.print("Enter a key: ");
+      System.out.print("[-] Enter a key: ");
       key = kb.nextLine();
     } catch (InputMismatchException e) {
-      System.out.println("Invalid input! Please enter a valid key.");
+      System.out.println("[!] Invalid input! Please enter a valid key.");
       decryptMenu();
     }
 
     // Reading the given file
-    String fileContents = "";
-
-    try (Scanner inFile = new Scanner(new File(filename))) {
-      while (inFile.hasNextLine()) {
-        fileContents = inFile.nextLine();
-      }
-    } catch (FileNotFoundException e) {
-      System.out.println("Given file not found.");
-      startMenu();
-    }
+    String fileContents = readFile(filename);
 
     // Decrypting the file and placing into
     String deContents = aes.decrypt(fileContents, key);
@@ -146,36 +127,43 @@ public class Main {
       try {
         outFile.createNewFile();
       } catch (IOException e) {
-        System.out.println("Couldnt create new file!");
+        System.out.println("[!] Couldnt create new file!");
         startMenu();
       }
     }
 
-    try (PrintWriter w = new PrintWriter(new PrintWriter("plaintext.txt"))) {
-      w.println(deContents);
-      System.out.println("File has been encrypted successfully\nOutput is located in 'plaintext.txt'");
-    } catch (FileNotFoundException e) {
-      System.out.println("Given file not found.");
-      startMenu();
+    // Write to the file
+    if (writeFile("plaintext.txt", deContents)) {
+      System.out.println("[✓] File has been encrypted successfully\n[└] Output is located in 'plaintext.txt'");
+    } else {
+      System.out.println("[!] Couldn't write to file! Make sure theres a 'plaintext.txt' file in the directory.'");
     }
 
     startMenu();
   }
 
-  // Decryption the file
-  public static void decryptMenu() {
-    Scanner kb = new Scanner(System.in);
-    AES aes = new AES();
-    String filename = "";
+  // Function to read the contents of the given file
+  public static String readFile(String filename) {
+    String fileContent = "";
 
-    // Validating the file name
-    try {
-      System.out.print("Enter a filename: ");
-      filename = kb.nextLine();
+    try (Scanner file = new Scanner(new File(filename))) {
+      while (file.hasNextLine()) {
+        fileContent = file.nextLine();
+      }
+    } catch (FileNotFoundException e) {
+      fileContent = "[!] Given file not found. Please check your filename and try again.";
+    }
 
-      // IF should go here to catch cases where user inputs wrong
-    } catch (InputMismatchException e) {
-      System.out.println("Invalid input! Please enter a valid file name.");
+    return fileContent;
+  }
+
+  // Function to write to a given file
+  public static boolean writeFile(String filename, String content) {
+    try (PrintWriter w = new PrintWriter(filename)) {
+      w.println(content);
+      return true;
+    } catch (FileNotFoundException e) {
+      return false;
     }
   }
 }
